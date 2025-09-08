4f0a2790f0809ad30cac7e84502a3fbffeca24b9
RafaelMotaAlvess <rmalves.geomais@gmail.com>
2025-09-08 00:59:26 -0300

    feat: add test execution with syntax analysis and error handling

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "gals/Lexico.h"
+#include "gals/Sintatico.h"
+#include "gals/Semantico.h"
+
+using namespace std;
+
+int main () {
+  Lexico lex; 
+  Sintatico sint;
+  Semantico sem;
+
+  lex.setInput("while (x < 10){read(x);}");
+
+  try {
+    sint.parse(&lex, &sem);
+    cout << "Analise concluida com sucesso!" << endl;
+  } catch (LexicalError err) {
+    cerr << "Problema lexico: " << err.getMessage() << endl;
+  } catch (SyntacticError err) {
+    cerr << "Problema sintatico: " << err.getMessage() << endl;
+  } catch (SemanticError err) {
+    cerr << "Problema semantico: " << err.getMessage() << endl;
+  }
+}
