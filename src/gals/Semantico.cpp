11b5716b12aff89387abd1d3775aa1abfc1bae17
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-10-08 21:15:15 -0300

    feat: add another cases of the lexema

--- src/gals/Semantico.cpp
+++ src/gals/Semantico.cpp
@@ -77,6 +77,66 @@ void Semantico::executeAction(int action, const Token *token)
     case 22:
       // ATTRIBUTION
       break;
+    case 23:
+      // FUNCTION DECLARATION
+      break;
+    case 24:
+      // INCREMENT/DECREMENT
+      break;
+    case 25:
+      // CONST/VAR
+      break;
+    case 26:
+      // MULTI VAR DECLARATION
+      break;
+    case 27:
+      // ATTRIBUTION INCREMENT/DECREMENT  
+      break;
+    case 28:
+      // OPEN BRACKET INDEX
+      break;
+    case 29:
+      // CLOSE BRACKET INDEX
+      break;
+    case 30:
+      // FUNCTION TYPE
+      break;
+    case 31:
+      // RETURN
+      break;
+    case 32:
+      // BREAK
+      break;
+    case 33:
+      // THROW
+      break;
+    case 34:
+      // IF/ELIF/ELSE
+      break;
+    case 35:
+      // DO
+      break;
+    case 36:
+      // WHILE
+      break;
+    case 37:
+      // FOR
+      break;
+    case 38:
+      // FOR VARIABLE
+      break;
+    case 39:
+      // FOR VARIABLE TYPE
+      break;
+    case 40:
+      // FOR VALUE
+      break;
+    case 41:
+      // SWITCH/CASE/DEFAULT
+      break;
+    case 99:
+      // FINAL CODE
+      break;
     default:
       cout << "Unknown semantic action" << endl;
       break;
