c06e312c470f41212299ba49d7f8281d2e08e2f2
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-10-12 18:03:59 -0300

    feat: update constants file

--- src/gals/Constants.h
+++ src/gals/Constants.h
@@ -98,10 +98,10 @@ const int ACCEPT = 3;
 const int GO_TO  = 4;
 const int ERROR  = 5;
 
-extern const int PARSER_TABLE[366][248][2];
+extern const int PARSER_TABLE[368][248][2];
 
 extern const int PRODUCTIONS[174][2];
 
-extern const char *PARSER_ERROR[366];
+extern const char *PARSER_ERROR[368];
 
 #endif
