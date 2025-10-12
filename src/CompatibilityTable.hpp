f304ceae895c47de325e6f211b64951d48d49c29
RafaelMotaAlvess <rmalves.geomais@gmail.com>
2025-10-12 14:57:34 -0300

    refactor: remove CompatibilityTable and related enums

--- src/CompatibilityTable.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "enums/AttributeResult.hpp"
-#include "enums/Operator.hpp"
-#include "enums/Type.hpp"
-
-Type resultType(Type left, Type right, Operator op);
-AttributeResult attributeResult(Type left, Type right);
-
